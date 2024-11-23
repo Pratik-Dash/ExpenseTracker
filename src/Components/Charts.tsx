b3ab4aca2635c1efaa4bd484447bf9b1a12c5208
Pratik Dash <dashpratik40@gmail.com>
2024-11-23 20:33:57 +0530

    FEAT: Added data visualization through charts

--- src/Components/Charts.tsx
+++ src/Components/Charts.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Chart from "chart.js/auto";
-import { CategoryScale } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import { ArcElement, CategoryScale } from "chart.js";
+import { Line, Pie } from "react-chartjs-2";
+import { ExpenseContext } from '../Context/AppContext';
+import { Category } from '@mui/icons-material';
+
 export const Data = [
     {
       id: 1,
@@ -34,44 +37,123 @@ export const Data = [
       userLost: 234
     }
   ];
-  Chart.register(CategoryScale);
+
+  interface Chartdataset{
+
+    label:string,
+    data:number[],
+    backgroundColor:string[],
+    borderWidth:number
+  }
+  interface ChartDataType{
+    labels:string[],
+    datasets:Chartdataset[]
+ }
+ interface LineDataset{
+    label:string,
+    data:number[],
+    fill:boolean,
+    borderColor:string,
+    backgroundColor:string,
+    tension:number
+ }
+ interface LinedataType{
+
+    labels:string[],
+    datasets:LineDataset[]
+ }
 const Charts = () => {
+    Chart.register(ArcElement)
+    const [categoryWiseData,setCategoryWiseData] = useState<Record<string,number>>({})
+    const [chartData, setChartData] = useState<ChartDataType>(
+        {
+            labels:[],
+            datasets: [
+                {
+                  label: 'Expenses by Category',
+                  data: [],
+                  backgroundColor: [
+                    'rgba(255, 255, 255, 0.6)',
+                    'rgba(255, 255, 255, 0.6)',
+                    'rgba(255, 255, 255, 0.6)',
+                  ],
+                  borderWidth: 1,
+                }
+            ]
+    }
+    )
+    const [lineData,setLineData] = useState<LinedataType>(
+        {
+            labels: [],
+            datasets: [{
+              label: 'Expenses by Category',
+              data: [],
+              fill: true,
+              borderColor: 'rgb(75, 192, 192)',
+              backgroundColor:'rgb(75, 192, 192,.6)',
+              tension: 0.1
+            }]
+          }
+    )
+    const context = useContext(ExpenseContext);
+    if(!context){
+        throw new Error("New Error");
+    }
+    const {expenses} = context;
     
-    const data = {
-        labels: ['Red', 'Orange', 'Blue'],
-        // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
-        datasets: [
+    useEffect(() => {
+
+        const categorywiseSumAmount = expenses.reduce<Record<string,number>>((resultObject, {category,amount}) => {
+
+            resultObject[category] = (resultObject[category]? resultObject[category]:0) + amount
+            return resultObject
+        },{})
+
+        setCategoryWiseData(categorywiseSumAmount)
+        
+       
+        setChartData(
             {
-              label: 'Popularity of colours',
-              data: [55, 23, 96],
-              // you can set indiviual colors for each bar
-              backgroundColor: [
-                'rgba(255, 255, 255, 0.6)',
-                'rgba(255, 255, 255, 0.6)',
-                'rgba(255, 255, 255, 0.6)',
-              ],
-              borderWidth: 1,
-            }
-        ]
-}
-const [chartData, setChartData] = useState({
-    labels: Data.map((data) => data.year), 
-    datasets: [
-      {
-        label: "Users Gained ",
-        data: Data.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2
-      }
-    ]
-})
+                
+                labels:Object.keys(categorywiseSumAmount),
+                datasets: [
+                    {
+                      label: 'Expenses by Category',
+                      data: Object.values(categorywiseSumAmount),
+                      backgroundColor: [
+                        'rgba( 86, 101, 115,0.8 )',
+                        'rgba( 39, 55, 70, 0.8 )',
+                        'rgba(28, 40, 51, 0.8)',
+                      ],
+                      borderWidth: 1,
+                    }
+                ]
+        }
+        )
+        setLineData(
+            {
+                labels: Object.keys(categorywiseSumAmount),
+                datasets: [{
+                  label: 'Expenses by Category',
+                  data: Object.values(categorywiseSumAmount),
+                  fill: true,
+                  borderColor: 'rgba(28, 40, 51, 0.8)',
+                  backgroundColor:'rgb(128, 139, 150,.4)',
+                  tension: 0.1
+                }]
+              }
+        )
+        
+    },[expenses])
+    
+    
+    console.log(categoryWiseData)
+    console.log(chartData)
+
+    
+
   return (
+    
     <div className="chart-container">
       <h2 style={{ textAlign: "center" }}>Pie Chart</h2>
       <Pie
@@ -80,11 +162,12 @@ const [chartData, setChartData] = useState({
           plugins: {
             title: {
               display: true,
-              text: "Users Gained between 2016-2020"
+              text: "Expenses across all categories"
             }
           }
         }}
       />
+      <Line data={lineData}/>
     </div>
   )
 }
